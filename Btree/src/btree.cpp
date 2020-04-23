0c357069b390ae2c01d88a446e4a58aa8472095c
msilveus1 <msilveus@wisc.edu>
2020-04-23 10:58:22 -0500

    test

--- Btree/src/btree.cpp
+++ Btree/src/btree.cpp
@@ -277,7 +277,7 @@ const void BTreeIndex::correctHeight(){
 	
 	// int isLeaf = 0;
 	for(int i = 0; i < height - 1; i++){
-		int tempValue = (int) pow((INTARRAYNONLEAFSIZE + 1),i+1)
+		int tempValue = (int) pow((INTARRAYNONLEAFSIZE + 1),i+1);
 		PageId currentArray[tempValue] = {};
 		int j = 0;
 		while(!queueList.isEmpty()){
