ad736ce141489bee92f70ba58785442e1e07c0f1
msilveus1 <msilveus@wisc.edu>
2020-04-22 21:26:10 -0500

    tenth set of error corrections

--- Btree/src/btree.h
+++ Btree/src/btree.h
@@ -307,7 +307,7 @@ class BTreeIndex {
   const void moveKeyIndex(int keyArray[],int size,int index);
   const void moveRecordIndex(RecordId* recordIdArray,int size,int index);
   const int split(void *childNode,int isLeaf, PageId &newID,PageId currentId,int keyValue, RecordId rid,PageId childPageId_1,PageId childPageId_2)
-  const void correctH;eight()
+  const void correctHeight()
   // class duplicateKeyException: public exception {
   //   const char * what() const throw (){
   //     return "C++ Exception";
